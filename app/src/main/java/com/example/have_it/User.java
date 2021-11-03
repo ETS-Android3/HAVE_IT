aee1e7a596472e12d8995f5e7aa77b1a8fee7196
allenhonggod <57022683+allenhonggod@users.noreply.github.com>
2021-11-02 23:27:37 -0600

    testFiles including the login UI

---
+++ app/src/main/java/com/example/have_it/User.java
@@ -0,0 +1,47 @@
+package com.example.have_it;
+
+/**
+ *This is the class using Singleton to share the current logged in user information
+ * @author yulingshen
+ */
+public class User {
+    /**
+     *The User ID, generated when registered, of class {@link String}
+     */
+    private String UID;
+    /**
+     *An instance to finalize this to be the only user among this app, of class {@link User}
+     */
+    private static final User instance = new User();
+
+    /**
+     *This is the constructor of {@link User}, private as others should not invoke this
+     */
+    private User(){
+        UID = "";
+    }
+
+    /**
+     *This is for getting the instance of user
+     * @return {@link User} the current user
+     */
+    public static User getInstance(){
+        return instance;
+    }
+
+    /**
+     *This is getter for UID
+     * @return {@link String}, get {@link User#UID}
+     */
+    public String getUID() {
+        return UID;
+    }
+
+    /**
+     *This is setter for UID
+     * @param UID {@link String}, set {@link User#UID}
+     */
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+}
