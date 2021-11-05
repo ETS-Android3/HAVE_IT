850f74d557c980ab11bd7081a7bacf44b8f9a1e1
YulingShen <yshen1@ualberta.ca>
2021-11-04 22:28:39 -0600

    Change in test of habit

--- app/src/test/java/com/example/have_it/HaveItUnitTest.java
+++ app/src/test/java/com/example/have_it/HaveItUnitTest.java
@@ -3,6 +3,10 @@ package com.example.have_it;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -68,27 +72,35 @@ public class HaveItUnitTest {
     }
 
 
-/*    @Test
+    @Test
     public void TestHabitInitialization(){
         testHabit = new Habit("play League", "I don't want to sleep",
                 Calendar.getInstance().getTime(), new ArrayList<>(Arrays.asList(false, false, false, false, false, false, false)));
         assertEquals("play League", testHabit.getTitle());
         assertEquals("I don't want to sleep", testHabit.getReason());
-        *//*assertEquals("2021-11-11", testHabit.getDateStart().toString());*//*
+        assertEquals(Calendar.getInstance().getTime().toString(), testHabit.getDateStart().toString());
         assertEquals(7, testHabit.getWeekdayReg().size());
     }
 
     @Test
     public void TestsetHabit(){
-      testHabit = new Habit("play League", "I don't want to sleep",
+        testHabit = new Habit("play League", "I don't want to sleep",
                 Calendar.getInstance().getTime(), new ArrayList<>(Arrays.asList(false, false, false, false, false, false, false)));
         testHabit.setTitle("stay awake");
         testHabit.setReason("I don't want to play league");
         testHabit.setWeekdayReg(new ArrayList<>(Arrays.asList(false)));
+        Date testDate = new Date();
+        try {
+            testDate = new SimpleDateFormat("yyyy-MM-dd")
+                    .parse("2021-11-11");
+        } catch (ParseException e){
+        }
+        testHabit.setDateStart(testDate);
         assertEquals("stay awake", testHabit.getTitle());
         assertEquals("I don't want to play league", testHabit.getReason());
+        assertEquals("2021-11-11", new SimpleDateFormat("yyyy-MM-dd").format(testHabit.getDateStart()));
         assertEquals(1, testHabit.getWeekdayReg().size());
-    }*/
+    }
 
     @Test
     public void TestHabitListInitialize(){
